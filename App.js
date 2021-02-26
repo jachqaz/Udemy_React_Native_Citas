9d0b87fb6dce908f8f46cdb7a7324cb6c46495d6
Johan Castano <johan.castano@arkangroup.com>
2021-02-26 09:08:13 -0500

    25. Creando el Primer Proyecto y Primeros Pasos
    Signed-off-by: Johan Castano <johan.castano@arkangroup.com>

--- App.js
+++ App.js
@@ -1,28 +1,12 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { Text } from "react-native";
 
 const App: () => React$Node = () => {
-  console.log("React Native1");
   return (
     <>
-      <Text style={styles.encabezado}>Hola mundo!</Text>
+      <Text>Proyecto Citas</Text>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  encabezado: {
-    textAlign: "center",
-    marginTop: 100,
-  },
-});
 
 export default App;
