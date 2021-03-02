7584b14c0c0bce7c1026a8b8a806aaa19d48a777
Johan Castano <johan.castano@arkangroup.com>
2021-03-02 16:56:50 -0500

    45. Mostrando de forma condicional el Formulario
    Signed-off-by: Johan Castano <johan.castano@arkangroup.com>

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, Platform, StyleSheet, Text, TouchableHighlight, View } from "react-native";
 import Cita from "./componentes/Cita";
 import Formulario from "./componentes/Formulario";
 
 const App = () => {
+  const [mostrarForm, guardarMostrarForm] = useState(false);
   const [citas, setCitas] = useState([
     {
       id: "1",
@@ -32,22 +33,44 @@ const App = () => {
     });
   };
 
+  //Muestra u oculta el formulario
+  const mostrarFormulario = () => {
+    guardarMostrarForm(!mostrarForm);
+  };
+
   return (
     <View style={styles.contenedor}>
       <Text style={styles.titulo}>Administrador de Citas</Text>
+      <View>
+        <TouchableHighlight
+          onPress={() => mostrarFormulario()}
+          style={styles.btnMostrarForm}>
+          <Text style={styles.textoMostrarForm}>Crear Nueva Cita</Text>
+        </TouchableHighlight>
+      </View>
       <View style={styles.contenido}>
-        <Formulario />
-        <Text style={styles.titulo}>
-          {citas.length > 0 ? "Administra tus citas" : "No hay citas, Agrega una"}
-        </Text>
-        <FlatList
-          style={styles.listado}
-          data={citas}
-          renderItem={({ item }) => (
-            <Cita item={item} eliminarPaciente={eliminarPaciente} />
-          )}
-          keyExtractor={(cita) => cita.id}
-        />
+        {mostrarForm ? (
+          <>
+            <Text style={styles.titulo}>Crear Nueva Cita</Text>
+            <Formulario />
+          </>
+        ) : (
+          <>
+            <Text style={styles.titulo}>
+              {citas.length > 0
+                ? "Administra tus citas"
+                : "No hay citas, Agrega una"}
+            </Text>
+            <FlatList
+              style={styles.listado}
+              data={citas}
+              renderItem={({ item }) => (
+                <Cita item={item} eliminarPaciente={eliminarPaciente} />
+              )}
+              keyExtractor={(cita) => cita.id}
+            />
+          </>
+        )}
       </View>
     </View>
   );
@@ -60,7 +83,7 @@ const styles = StyleSheet.create({
   },
   titulo: {
     color: "#FFF",
-    marginTop: 40,
+    marginTop: Platform.OS === "android" ? 20 : 40,
     marginBottom: 20,
     fontSize: 24,
     fontWeight: "bold",
@@ -72,6 +95,16 @@ const styles = StyleSheet.create({
   },
   listado: {
     flex: 1,
+  },
+  btnMostrarForm: {
+    padding: 10,
+    backgroundColor: "#7d024e",
+    marginVertical: 10,
+  },
+  textoMostrarForm: {
+    color: "#fff",
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
 
